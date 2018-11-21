9da39db7fa74042135cf6f032a4d255f821f8d34
nvoglaender <nvogtlaender@wedacon.net>
2018-11-21 12:55:18 +0100

    added test components

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,15 +4,63 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AddressFormComponent } from './testcomponents/address-form/address-form.component';
+import { MatInputModule,
+  MatButtonModule,
+  MatSelectModule,
+  MatRadioModule,
+  MatCardModule,
+  MatToolbarModule,
+  MatSidenavModule,
+  MatIconModule,
+  MatListModule,
+  MatTableModule,
+  MatPaginatorModule,
+  MatSortModule,
+  MatGridListModule,
+  MatMenuModule,
+  MatTreeModule } from '@angular/material';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NavComponent } from './testcomponents/nav/nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { TableComponent } from './testcomponents/table/table.component';
+import { DashboardComponent } from './testcomponents/dashboard/dashboard.component';
+import { TreeComponent } from './testcomponents/tree/tree.component';
+import { DragdropComponent } from './testcomponents/dragdrop/dragdrop.component';
+import { DragDropModule } from '@angular/cdk/drag-drop';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddressFormComponent,
+    NavComponent,
+    TableComponent,
+    DashboardComponent,
+    TreeComponent,
+    DragdropComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatRadioModule,
+    MatCardModule,
+    ReactiveFormsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatGridListModule,
+    MatMenuModule,
+    MatTreeModule,
+    DragDropModule
   ],
   providers: [],
   bootstrap: [AppComponent]
