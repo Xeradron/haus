788779909fab89c49dc65142e95ea0aa13756351
Xeradron <nino.vogtlaender@web.de>
2019-07-04 12:31:46 +0200

    first CRUD test

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,12 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatGridListModule, MatCardModule, MatMenuModule, MatIconModule, MatButtonModule } from '@angular/material';
 import { LayoutModule } from '@angular/cdk/layout';
 import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { MatInputModule } from '@angular/material/input';
+import { MatGridListModule, MatCardModule, MatMenuModule, MatIconModule, MatButtonModule, MatTableModule } from '@angular/material';
 
 import { TodoTestComponent } from './todo-test/todo-test.component';
 
@@ -26,7 +30,11 @@ import { TodoTestComponent } from './todo-test/todo-test.component';
     MatIconModule,
     MatButtonModule,
     LayoutModule,
-    HttpClientModule
+    HttpClientModule,
+    MatTableModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- client/src/app/todo-test/todo-test.component.ts
+++ client/src/app/todo-test/todo-test.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { TodoControllerService, InlineResponse200 } from '../api_v4';
-import { Observable } from 'rxjs';
+import { TodoControllerService, InlineResponse200, Todo } from '../api_v4';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-todo-test',
@@ -10,18 +10,67 @@ import { Observable } from 'rxjs';
 })
 export class TodoTestComponent implements OnInit {
 
-  todoService: TodoControllerService
-  todoCount
+  private todoService: TodoControllerService
+
+  private todoCount: number;
+  private todos: Todo[];
+  private displayedCoulmns = ["id","title", "desc"];
+  private selectedID: number;
+  private todoFormGroup: FormGroup;
 
   constructor(todoService: TodoControllerService) {
     this.todoService = todoService;
+
+    this.todoFormGroup = new FormGroup({
+      "title": new FormControl(),
+      "desc": new FormControl()
+    });
   }
 
   ngOnInit() {
-    this.todoService.todoControllerCount(null, 'body').subscribe((value) => {
-      this.todoCount = value;
-      console.log(value);
-    })
+    this.getTodoCount();
+    this.getTodoList();
+  }
+
+  getTodoByID(id: number) {
+    this.selectedID = id;
+    this.todoService.todoControllerFindById(id, "body").subscribe((response: Todo) => {
+      this.todoFormGroup.controls["title"].setValue(response.title);
+      this.todoFormGroup.controls["desc"].setValue(response.desc);
+    });
+  }
+
+  getTodoList () {
+    this.todoService.todoControllerFind(null,"body").subscribe((response: Todo[]) => {
+      this.todos = response;
+    });
+  }
+
+  getTodoCount() {
+    this.todoService.todoControllerCount(null, 'body').subscribe((response: InlineResponse200) => {
+      this.todoCount = response.count;
+    });
+  }
+
+  updateTodobyID(id: number) {
+    this.todoService.todoControllerUpdateById(id, this.todoFormGroup.value, "body").subscribe((response) => {
+      this.getTodoList();
+    });
+  }
+
+  deleteTodoByID(id: number) {
+    this.todoService.todoControllerDeleteById(id,"body").subscribe(() => {
+      this.getTodoList();
+      this.getTodoCount();
+    });
+  }
+
+  addTodo() {
+    this.todoService.todoControllerCreate(this.todoFormGroup.value, "body").subscribe((response: Todo) => {
+      this.getTodoList();
+      this.getTodoCount();
+      this.getTodoByID(response.id);
+    });
   }
 
 }
